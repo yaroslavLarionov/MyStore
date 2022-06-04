177f61f013b3e6aeb4ba08b709befebc7ab30180
Banditphak <banditphak@hotmail.com>
2022-06-04 10:21:49 -0400

    Data

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -1,4 +1,21 @@
 package pages;
 
-public class HomePage {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class HomePage extends BasePage {
+    protected WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath = "")
+    public List<WebElement> footerLink;
 }
--- src/test/tests/HomeTest.java
+++ src/test/tests/HomeTest.java
@@ -1,4 +1,22 @@
 package tests;
 
-public class HomeTest {
-}
+import base.BaseTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class HomeTest extends BaseTest {
+    HomePage homePage;
+
+    @BeforeMethod
+    public void localSetUp(){
+        homePage = new HomePage(getDriver());
+    }
+    @DataProvider(name = "footer")
+        public Object[] testData() {
+        Object[] footerData ={""};
+        return footerData;
+    }
+    @Test(testName = "AUT-3 Footer - Information section", description = "Information section should have following buttons")
+
